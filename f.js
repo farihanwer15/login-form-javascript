8614a482d5242a18f468eb2dbec8df50b26d0317
farihaanwer15 <farihaanwer15@hotmail.com>
2021-09-29 15:02:01 +0500

    created form

---
+++ f.js
@@ -0,0 +1,9 @@
+var toggles = document.querySelectorAll('.faq-toggle')
+
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () =>
+    {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
